492fc16863ff44a471f6cd532f9e86c2edbce1bb
Artem Anashchenkov <sir13tommy@gmail.com>
2023-01-06 12:13:02 +0200

    Router adjusment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,29 @@ import { ContactMeComponent } from './contact-me/contact-me.component';
 import { HomeComponent } from './home/home.component';
 import { MyWorksComponent } from './my-works/my-works.component';
 
-const routes: Routes = [{
-  path: 'home',
-  component: HomeComponent
-}, {
-  path: 'myworks',
-  component: MyWorksComponent
-}, {
-  path: 'contact',
-  component: ContactMeComponent
-}, {
-  path: '*',
-  redirectTo: '/home'
-}];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'myworks',
+    component: MyWorksComponent
+  },
+  {
+    path: 'contact',
+    component: ContactMeComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
